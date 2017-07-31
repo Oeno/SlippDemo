cde3297cec7c0f8bf776d0d8920ce83bbbd26f1b
Songyeol Lee <soye73@naver.com>
2017-07-31 16:23:20 +0900

    Initial commit

---
+++ src/main/java/com/oeno/codesquad/CodesquadApplication.java
@@ -0,0 +1,12 @@
+package com.oeno.codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesquadApplication.class, args);
+	}
+}
