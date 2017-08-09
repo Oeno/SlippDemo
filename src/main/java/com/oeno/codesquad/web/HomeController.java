50b57f86203717ff2409458b3688f9cc5cde7d5b
Songyeol Lee <soye73@naver.com>
2017-08-09 11:29:54 +0900

    Modify print question list to reverse

--- src/main/java/com/oeno/codesquad/web/HomeController.java
+++ src/main/java/com/oeno/codesquad/web/HomeController.java
@@ -1,10 +1,14 @@
 package com.oeno.codesquad.web;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.oeno.codesquad.domain.Question;
 import com.oeno.codesquad.domain.QuestionRepository;
 
 @Controller
@@ -16,7 +20,20 @@ public class HomeController {
 	@GetMapping("/")
 	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView("/index");
-		mav.addObject("questions", questionRepository.findAll());
+		
+		mav.addObject("questions", reverseQuestion(questionRepository.findAll()));
 		return mav;
+	}
+	
+	// Question 목록 역순으로 변환
+	public ArrayList<Question> reverseQuestion(Iterable<Question> questions) {
+		ArrayList<Question> questionList = new ArrayList<>();
+		
+		for (Question question : questions) {
+			questionList.add(question);
+		}
+		Collections.reverse(questionList);
+		
+		return questionList;
 	}
 }
